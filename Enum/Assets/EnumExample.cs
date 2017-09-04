fa377990018da435e76205fff8065ef7f10f9a81
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-09-04 17:46:34 +0300

    Enum
    https://www.youtube.com/watch?v=j7PluuoGtC8&index=2&list=PLadYLGMfR6Lp0UkcG6DJWD_Qaqzm4t--w

---
+++ Enum/Assets/EnumExample.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnumExample : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        Searching,
+        Attacking,
+        Dead
+    }
+
+    public EnemyState currentEnemyState;
+
+    void Start()
+    {
+        currentEnemyState = EnemyState.Searching;
+    }
+
+    void Update()
+    {
+        switch (currentEnemyState)
+        {
+            case EnemyState.Attacking:
+                Debug.Log("Attacking");
+                break;
+            case EnemyState.Searching:
+                Debug.Log("Searching");
+                break;
+            case EnemyState.Dead:
+                Debug.Log("Dead");
+                break;
+        }
+    }
+}
---
+++ Enum/Assets/InputScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputScript : MonoBehaviour
+{
+    private EnumExample _enumExample;
+  
+    void Start()
+    {
+        _enumExample = GetComponent<EnumExample>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _enumExample.currentEnemyState = (EnumExample.EnemyState)Random.Range(0, 3);
+        }
+    }
+}
