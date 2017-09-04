0bbee96cdc540369c55bfef7b4fb3c2536324cd8
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-09-04 18:19:30 +0300

    Lists
    https://www.youtube.com/watch?v=p-KjunKkuJw&index=1&list=PLadYLGMfR6Lp0UkcG6DJWD_Qaqzm4t--w

---
+++ Lists/Assets/ListExample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+public class ListExample : MonoBehaviour
+{
+    public List<int> ages = new List<int>();
+    public List<GameObject> objects = new List<GameObject>();
+    public List<string> myStrings = new List<string>();
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ages.Add(Random.Range(1, 100));
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ages.Remove(ages[Random.Range(0, ages.Count)]);
+        }
+    }
+}
