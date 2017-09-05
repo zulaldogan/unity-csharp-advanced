7456cd45afcfab12558f48144c260530c487e904
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-09-05 12:55:12 +0300

    Structs & Classes v2
    https://www.youtube.com/watch?v=G5KJolM2uzs&list=PLadYLGMfR6Lp0UkcG6DJWD_Qaqzm4t--w&index=3

---
+++ Structs & Classes v2/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Book
+{
+    public int price;
+    public string title;
+    public string author;
+
+    public Book(int p, string t, string a)
+    {
+        this.price = p;
+        this.title = t;
+        this.author = a;
+    }
+}
+
+[System.Serializable]
+public struct Game
+{
+    public int gamePrice;
+    public int company;
+    public int platform;
+}
+
+public class Player : MonoBehaviour
+{
+    public Book UnityProgramming = new Book(30,"Unity 3D","W.W.T");
+    public Game UnityGaming;
+}
