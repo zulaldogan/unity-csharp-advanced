f2d87b6072123329bd6179624977724cd7db280b
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-09-05 14:44:16 +0300

    NullableTypes
    https://www.youtube.com/watch?v=lK4iK_ZuBTM&index=4&list=PLadYLGMfR6Lp0UkcG6DJWD_Qaqzm4t--w

---
+++ NullableTypes/Assets/NullableTypes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class NullableTypes : MonoBehaviour
+{
+    void Start()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) ;
+        {
+            CreateUser("Unity 3D", null, true);
+        }
+    }
+
+    void CreateUser(string name, int? age, bool isCool)
+    {
+        int finalAge = age ?? 26;
+        Debug.Log("Name: " + name);
+        Debug.Log(("IsCool :" + isCool));
+        Debug.Log("Age :" + finalAge);
+    }
+}
